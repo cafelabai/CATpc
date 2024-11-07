3feb8f2043169240260a424acb11408084adc28d
Jeremy Price <jfprice@iu.edu>
2024-11-07 13:22:11 -0500

    citation added

---
+++ CITATION.cff.c
@@ -0,0 +1,6 @@
+@software{Jadhav_Catpc_2024,
+  author = {Jadhav, Kshitija Suresh and Chauhan, Ankit Singh and Chakraborty, Sunandan and Price, Jeremy F},
+  title = {{CATpc}},
+  year = {2024},
+  url = {https://github.com/cafelabai/CATpc}
+}
